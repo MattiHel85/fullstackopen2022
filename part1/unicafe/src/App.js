1a436b619c3238954334e153615efa384d8ea961
Matt Simpson <matt.rc.simpson@gmail.com>
2022-06-29 12:06:04 +0300

    complete unicafe exercises

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,19 +1,55 @@
 import React, { useState } from 'react'
 import './App.css';
 
-const Button = ({text, handleClick}) => (<button>{text}</button>)
+const Button = ({text, handleClick}) => (<button onClick={handleClick}>{text}</button>)
+const Stat = ({label, amount}) => {
+  return(
+    <tr>
+      <td>{label}</td>
+      <td>{amount}</td>
+    </tr>
+  )
+}
+const Header = ({ heading }) => (<h1>{heading}</h1>)
 
 const App = () => {
+
   const [good, setGood ] = useState(0)
   const [neutral, setNeutral ] = useState(0)
   const [bad, setBad ] = useState(0)
+
+  const handleGood = () => {
+    setGood(good + 1)
+  }
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+  const handleBad = () => {
+    setBad(bad + 1)
+  }
+
   return (
     <div className="App">
-      <h1>Give Feedback</h1>
-      <Button text={"good"} />
-      <Button text={"neutral"} />
-      <Button text={"bad"} />
-      <h2>Statistics</h2>
+      <Header heading={"Give Feedback"} />
+      <Button handleClick={handleGood} text={"good"} />
+      <Button handleClick={handleNeutral} text={"neutral"} />
+      <Button handleClick={handleBad} text={"bad"} />
+      <p>
+        {(good + bad + neutral) >= 1 ? "" : "No feedback given"}
+      </p>
+      <table>
+        <thead>
+          <Header heading="Statistics" />
+        </thead>
+        <tbody>
+          <Stat label={good ? "good" : ""} amount={good ? good : ""}/>
+          <Stat label={neutral ? "neutral" : ""} amount={neutral ? neutral :""}/>
+          <Stat label={bad ? "bad" : ""} amount={bad ? bad : ""}/>
+          <Stat label={(good + bad + neutral) >= 1 ? "all" : ""} amount={(good + bad + neutral) >= 1 ? good + bad + neutral : ""} />
+          <Stat label={(good + bad + neutral) >= 1 ? "average" : ""} amount={(good + bad + neutral) >= 1 ? (good - bad)/(good + bad + neutral) : ""} />
+          <Stat label={good ? "positive" : ""} amount={good ? good/(good+bad+neutral) * 100 + "%" : ""} />
+        </tbody>
+      </table>
     </div>
   );
 }
