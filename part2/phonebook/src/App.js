a4dc77125c5e7f8b9687e3bb129b72339bf5e71f
Matt Simpson <matt.rc.simpson@gmail.com>
2022-07-03 11:05:30 +0300

    phone book exercise step 2 - incomplete

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,8 +3,33 @@ import './App.css';
 
 const App = () =>  {
   const [persons, setPersons] = useState([
-    {name: 'Arto Hellas'}
+    {
+      id: 1,
+      name: 'Arto Hellas'
+    },
+    {
+      id: 2,
+      name: 'Jorma Ori'
+    }
   ])
+
+  const [newName, setNewName] = useState('')
+
+  const addName = (e) => {
+    e.preventDefault()
+    setNewName(e.target.value)
+  }
+
+  const addPerson = (e) => {
+    e.preventDefault()
+    let personObject = {
+      id: persons.length + 1,
+      name: newName
+    }
+    persons.map(person => person.name === personObject.name ? alert(`${newName} is already added to phonebook`) : setPersons(persons.concat(personObject)))
+    setNewName('')
+  }
+
 
   return (
     <div className="App">
@@ -13,14 +38,17 @@ const App = () =>  {
       </h2>
       <form>
         <div>
-          name: <input />
-        </div>
-        <div>
-          <button type='submit'>
+          <span>Name: </span>
+          <input value={newName} onChange={addName} />
+          <button type='submit' onClick={addPerson}>
             add
           </button>
         </div>
       </form>
+      <h2>
+        Numbers
+      </h2>
+      {persons.map(person => <p key={person.id}>{person.name}</p>)}
     </div>
   );
 }
